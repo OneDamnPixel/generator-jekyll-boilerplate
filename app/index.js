02943c95fae58d696f549647bb3894dd13b08ac0
Daniel Reid <danielc.reid@gmail.com>
2015-11-03 15:00:54 -0600

    Better prompting for _config.yml

--- app/index.js
+++ app/index.js
@@ -1,42 +1,57 @@
 var generators = require('yeoman-generator');
 var mkdirp = require('mkdirp');
+var _ = require('lodash');
 
 module.exports = generators.Base.extend({
-
-  constructor: function () {
+  constructor: function() {
     generators.Base.apply(this, arguments);
-
-    // Add command line arguments here
-
-    // Uncomment this and Yeoman will allow for a blank name (allows you to use the current working directory)
-    //this.argument('appname', {
-    //  desc: 'The applications name. Cannot be blank.',
-    //  required: false,
-    //  type: String
-    //});
-
-    // Add command line options here
-    // this.option('scss'); // If you include --scss in the command line, the generator can use this.options.scss to do stuff (returns true/false)
   },
 
-
-  // Call via: this._privateMethod();
-  // _privateMethod: function(text) {
-  //   console.log(text);
-  // },
-
-  initializing: {},
+  initializing: function() {
+    this.props = {}
+  },
 
   prompting: function() {
     var done = this.async();
 
-    this.prompt({
-      type    : 'input',
-      name    : 'appname',
-      message : 'Your project name',
+    var prompts = [{
+      name: 'projectName',
+      message: 'Project name:',
       default : 'jekyll-boilerplate'
-    }, function (answers) {
-      this.appname = answers.appname.replace(/\s+/g, '-').toLowerCase(); // Convert to lower case and dash separated name
+    }, {
+      name: 'projectDescription',
+      message: 'Project description:'
+    }, {
+      name: 'projectURL',
+      message: 'Project URL:'
+    }, {
+      name: 'authorName',
+      message: 'Author name:',
+      store: true
+    }, {
+      name: 'authorEmail',
+      message: 'Author email:',
+      store: true
+    }, {
+      name: 'authorURI',
+      message: 'Author website:',
+      store: true
+    }, {
+      name: 'authorBio',
+      message: 'Author bio:',
+      store: true
+    }, {
+      name: 'authorTwitter',
+      message: 'Author Twitter username:',
+      store: true
+    }, {
+      name: 'authorGithub',
+      message: 'Author GitHub username:',
+      store: true
+    }];
+
+    this.prompt(prompts, function(props) {
+      this.props = _.assign(this.props, props);
       done();
     }.bind(this));
   },
@@ -45,16 +60,17 @@ module.exports = generators.Base.extend({
 
   default: {},
 
-  writing: function () {
+  writing: function() {
+    var destinationPathRoot = this.props.projectName.replace(/\s+/g, '-').toLowerCase();
 
-    mkdirp('./' + this.appname + '/_posts');
-    mkdirp('./' + this.appname + '/_sass');
-    mkdirp('./' + this.appname + '/css');
+    mkdirp(destinationPathRoot + '/_posts');
+    mkdirp(destinationPathRoot + '/_sass');
+    mkdirp(destinationPathRoot + '/css');
 
     // Includes
     this.fs.copyTpl(
       this.templatePath('_includes/head.html'),
-      this.destinationPath('./' + this.appname + '/_includes/head.html'),
+      this.destinationPath(destinationPathRoot + '/_includes/head.html'),
       {}
     );
 
@@ -63,19 +79,19 @@ module.exports = generators.Base.extend({
     // Layouts
     this.fs.copyTpl(
       this.templatePath('_layouts/default.html'),
-      this.destinationPath('./' + this.appname + '/_layouts/default.html'),
+      this.destinationPath(destinationPathRoot + '/_layouts/default.html'),
       {}
     );
 
     this.fs.copyTpl(
       this.templatePath('_layouts/page.html'),
-      this.destinationPath('./' + this.appname + '/_layouts/page.html'),
+      this.destinationPath(destinationPathRoot + '/_layouts/page.html'),
       {}
     );
 
     this.fs.copyTpl(
       this.templatePath('_layouts/page.html'),
-      this.destinationPath('./' + this.appname + '/_layouts/page.html'),
+      this.destinationPath(destinationPathRoot + '/_layouts/page.html'),
       {}
     );
 
@@ -84,7 +100,7 @@ module.exports = generators.Base.extend({
     // Posts
     this.fs.copyTpl(
       this.templatePath('_layouts/default.html'),
-      this.destinationPath('./' + this.appname + '/_layouts/default.html'),
+      this.destinationPath(destinationPathRoot + '/_layouts/default.html'),
       {}
     );
 
@@ -93,47 +109,42 @@ module.exports = generators.Base.extend({
     // Root
     this.fs.copyTpl(
       this.templatePath('.gitignore'),
-      this.destinationPath('./' + this.appname + '/.gitignore'),
+      this.destinationPath(destinationPathRoot + '/.gitignore'),
       {}
     );
 
     this.fs.copyTpl(
       this.templatePath('_config.yml'),
-      this.destinationPath('./' + this.appname + '/_config.yml'),
-      {}
+      this.destinationPath(destinationPathRoot + '/_config.yml'),
+      {
+        projectName: this.props.projectName,
+        projectDescription: this.props.projectDescription,
+        projectURL: this.props.projectURL,
+        authorName: this.props.authorName,
+        authorEmail: this.props.authorEmail,
+        authorURI: this.props.authorURI,
+        authorBio: this.props.authorBio,
+        authorTwitter: this.props.authorTwitter,
+        authorGithub: this.props.authorGithub
+      }
     );
 
     this.fs.copyTpl(
       this.templatePath('feed.xml'),
-      this.destinationPath('./' + this.appname + '/feed.xml'),
+      this.destinationPath(destinationPathRoot + '/feed.xml'),
       {}
     );
 
     this.fs.copyTpl(
       this.templatePath('index.html'),
-      this.destinationPath('./' + this.appname + '/index.html'),
+      this.destinationPath(destinationPathRoot + '/index.html'),
       {}
     );
-
   },
 
   conflicts: {},
 
-  install: function () {
-    // Installs npm
-    //this.npmInstall();
+  install: function() {},
 
-    // Installs Bower
-    //this.bowerInstall();
-
-    // Installs both Bower and npm
-    //this.installDependencies();
-
-    // Bower requires bower.json with appropriate information
-    // npm requires package.json with appropriate information
-
-  },
-
-  end: function () {}
-
+  end: function() {}
 });
